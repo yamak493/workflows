66745be632d2b9837905fe5e7a3e3cb6a29bb5c8
Yamada Keitarou <116993117+yamak493@users.noreply.github.com>
2025-04-05 22:06:35 +0900

    Update warp.sh

--- warp.sh
+++ warp.sh
@@ -1,9 +1,4 @@
-curl https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
-
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
-
 sudo apt update
 sudo apt install warp-cli
-
-warp-cli register
+warp-cli registration new
 warp-cli connect
